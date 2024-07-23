bd4cadc4892265cd66e51ef17927a42c5ec38cf0
Sharat Jacob <sharatjacob2@gmail.com>
2024-07-23 19:50:09 +0530

    made a cute explore box

--- src/mobileWelcome/mobileWelcome.js
+++ src/mobileWelcome/mobileWelcome.js
@@ -1,24 +1,9 @@
-// import { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
 import './mobileWelcome.css';
 import { ReactComponent as TwiceF } from './TWICE NAMED f.svg';
 import { ReactComponent as TwiceB } from './TWICE NAMED b.svg';
 import { ReactComponent as Centre } from './mobile Start Image.svg';
 
-
 const MobileWelcome = () => {
-
-    // const navigate = useNavigate();
-
-    // useEffect(() => {
-    //     const handleResize = () => {
-    //         if (window.innerWidth >= 800) {
-    //             navigate('/');
-    //         }
-    //     };
-    //     // Add event listener for window resize
-    //     window.addEventListener('resize', handleResize);
-    // }, [navigate]);
 
     document.body.classList.add('hide-scrollbars');
 
@@ -30,6 +15,9 @@ const MobileWelcome = () => {
                 <TwiceB className='mobile-backTitle titles' />
                 <Centre className='mobile-centreImage' />
                 <TwiceF className='mobile-frontTitle titles' />
+            </div>
+            <div className="mobile-explore">
+                <p>explore</p>
             </div>
         </div>
     );

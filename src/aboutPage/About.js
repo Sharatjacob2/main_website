23071b50a751393cffabdd9ed394a04e08b5d032
Sharat Jacob <sharatjacob2@gmail.com>
2024-10-16 03:17:16 +0530

    responsive debugging for about page

--- src/aboutPage/About.js
+++ src/aboutPage/About.js
@@ -10,6 +10,7 @@ import Footer from "../footer/Footer";
 
 const About = () => {
   const title = { front: AboutF, back: AboutB, centre: Sharat, color: '#2E294E', top: -9.375, right: 15.625, display: 'none' };
+  document.body.classList.remove('hide-scrollbars');
 
   return (
     <div className="About-Page" style={{ backgroundColor: '#2E294E' }}>

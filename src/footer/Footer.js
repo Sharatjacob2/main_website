3b5582116931f7606367f65a6e846917b36eacd4
Sharat Jacob <sharatjacob2@gmail.com>
2024-10-16 20:59:58 +0530

    added links for the footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,18 +1,33 @@
-import { ReactComponent as Instagram} from './Instagram.svg';
-import { ReactComponent as LinkedIn} from './LinkedIn.svg';
-import { ReactComponent as Twitter} from './Twitter.svg';
-import { ReactComponent as Mail} from './Mail.svg';
+import { ReactComponent as Instagram } from './Instagram.svg';
+import { ReactComponent as LinkedIn } from './LinkedIn.svg';
+import { ReactComponent as Twitter } from './Twitter.svg';
+import { ReactComponent as Mail } from './Mail.svg';
 import './Footer.css';
+
+const FooterIcon = (props) => {
+    const { link, svgName: SvgIcon } = props.item;
+    return (
+        <a href={link} target="_blank" rel="noopener noreferrer">
+            <SvgIcon className="footericons"/>
+        </a>
+    );
+}
+
 const Footer = (props) => {
     const color = props.color;
-    return ( 
+    const footericonslist = [
+        {link: "https://mail.google.com/mail/?view=cm&to=sharatjacob2@gmail.com", svgName: Mail},
+        {link: "https://www.linkedin.com/in/sharat-jacob-jacob/", svgName: LinkedIn},
+        {link: "https://www.instagram.com/sharatj_2/", svgName: Instagram},
+        {link: "https://x.com/thetwicenamed", svgName: Twitter},
+    ]
+    return (
         <div className="footer" style={{ backgroundColor: color }}>
-            <Mail className='footericons'/>
-            <LinkedIn className='footericons'/>
-            <Instagram className='footericons'/>
-            <Twitter className='footericons'/>
+            {footericonslist.map((footericon) => (
+                <FooterIcon item = {footericon}/>
+            ))}
         </div>
-     );
+    );
 }
- 
+
 export default Footer;

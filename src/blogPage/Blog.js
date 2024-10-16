0125e804edc5bdbe9fa791a803f030395761c823
Sharat Jacob <sharatjacob2@gmail.com>
2024-10-16 19:24:03 +0530

    added blog and weird page

--- src/blogPage/Blog.js
+++ src/blogPage/Blog.js
@@ -5,13 +5,36 @@ import {ReactComponent as BlogCentre} from './Blog Centre.svg';
 import './Blog.css';
 import Footer from "../footer/Footer";
 
+
+const BlogItem = (props) => {
+    const {title, preview, date} = props.item;
+    return (
+        <div className="blog-item">
+            <div className="blog-title">{title}</div>
+            <div className="blog-preview">{preview}</div>
+            <div className="blog-date">{date}</div>
+        </div>
+      );
+}
+ 
 const Blog = () => {
+    document.body.classList.remove('hide-scrollbars');
+
+    const blogitems = [
+        {title: "The lethality of training on human feedback", preview: "Human operators are fallible, breakable, and manipulable.  Human raters make systematic errors - regular, compactly describable, predictable errors.  To faithfully learn....", date: "13th JAN 2023"},
+        {title: "The lethality of training on human feedback", preview: "Human operators are fallible, breakable, and manipulable.  Human raters make systematic errors - regular, compactly describable, predictable errors.  To faithfully learn....", date: "13th JAN 2023"}
+    ]
+
     const title = {front: BlogF, back: BlogB, centre: BlogCentre, color: '#C5D86D', top: -11.719, width: 46.875, height: 46.875, right: 27.344};
     return (
         <div className="Blog-Page" style={{backgroundColor: '#C5D86D'}}>
-            <TitleEffect title ={title}/>
+            <TitleEffect className="title-margin" title ={title}/>
+            <div className="blog-main-title">POSTS</div>
+            <div className="blog-main-title-underline"></div>
+            {blogitems.map((blogitem) => (
+                <BlogItem item={blogitem}/>
+            ))}
             <Footer color = {'#B7A555'}/>
-
     </div>
       );
 }
--- src/weirdPage/Weird.js
+++ src/weirdPage/Weird.js
@@ -2,15 +2,35 @@ import TitleEffect from "../TitleEffect";
 import {ReactComponent as WeirdF} from './WEIRD f.svg';
 import {ReactComponent as WeirdB} from './WEIRD b.svg';
 import {ReactComponent as WeirdCentre} from './Weird Test Scentre.svg';
+import {ReactComponent as WeirdLoader} from './Weird Loader.svg';
 import './Weird.css';
 import Footer from "../footer/Footer";
 
+const WeirdItem = (props) => {
+
+    const {svgNames: SvgIcon, description} = props.item;
+    return (
+        <div className="weird-item">
+            <SvgIcon className="weird-image"/>
+            <div className="weird-desc">{description}</div>
+        </div>
+      );
+}
+ 
+
 const Weird = () => {
     const title = {front: WeirdF, back: WeirdB, centre: WeirdCentre, left: 34.906, top: -5.8, width: 30, height: 30};
-
+    document.body.classList.remove('hide-scrollbars');
+    const weirditems = [
+        {svgNames: WeirdLoader, description: "a collection of pixel art made by coloring cells in  Google Sheet"},
+        {svgNames: WeirdLoader, description: "a collection of pixel art made by coloring cells in  Google Sheet"},
+    ]
     return (
         <div className="Weird-Page" style={{backgroundColor: '#1B998B'}}>
             <TitleEffect title ={title}/>
+            {weirditems.map((weirditem) => (
+                <WeirdItem item = {weirditem} />
+            ))}
             <Footer color = {'#062A26'}/>
 
     </div>

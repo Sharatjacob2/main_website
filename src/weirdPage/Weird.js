1c2178c059c5abef8de91461eada3bd499e86450
Sharat Jacob <sharatjacob2@gmail.com>
2024-10-16 22:07:35 +0530

    added images for weird items

--- src/weirdPage/Weird.js
+++ src/weirdPage/Weird.js
@@ -3,6 +3,10 @@ import {ReactComponent as WeirdF} from './WEIRD f.svg';
 import {ReactComponent as WeirdB} from './WEIRD b.svg';
 import {ReactComponent as WeirdCentre} from './Weird Test Scentre.svg';
 import {ReactComponent as WeirdLoader} from './Weird Loader.svg';
+import {ReactComponent as CircleArt} from './CircleArt.svg';
+import {ReactComponent as FigmaArt} from './FigmaArt.svg';
+import {ReactComponent as ActualPixelArt} from './ActualPixelArt.svg';
+
 import './Weird.css';
 import Footer from "../footer/Footer";
 
@@ -23,9 +27,9 @@ const Weird = () => {
     const title = {front: WeirdF, back: WeirdB, centre: WeirdCentre, left: 34.906, top: -5.8, width: 30, height: 30};
     document.body.classList.remove('hide-scrollbars');
     const weirditems = [
-        {svgNames: WeirdLoader, description: "a collection of pixel art made by coloring cells in  Google Sheet"},
-        {svgNames: WeirdLoader, description: "a collection of attempted modern art made with starter code from the p5js docs"},
-        {svgNames: WeirdLoader, description: "a collection of handdrawn animated videos made on flipaclip"},
+        {svgNames: ActualPixelArt, description: "a collection of pixel art made by coloring cells in  Google Sheet"},
+        {svgNames: CircleArt, description: "a collection of attempted modern art made with starter code from the p5js docs"},
+        {svgNames: FigmaArt, description: "a collection of digital designs made on Figma under an Unsplash craze"},
     ]
     return (
         <div className="Weird-Page" style={{backgroundColor: '#1B998B'}}>

1033b1f1bd902b0e3cab829d808139a0aea1f1de
Sharat Jacob <sharatjacob2@gmail.com>
2025-08-30 01:58:33 +0530

    updated BLOG with recent articles

--- src/blogPage/Blog.js
+++ src/blogPage/Blog.js
@@ -24,9 +24,16 @@ const Blog = () => {
     document.body.classList.remove('hide-scrollbars');
 
     const blogitems = [
+        { link: "https://alignedkitten.substack.com/p/interlude-my-possibly-controversial", title: "Interlude: My (Possibly Controversial) Takes on the AI Art Discourse", preview: "I strongly uphold your sentiment, but I probably disagree heavily with your argument.", date: "31st MAR 2025" },
+
         { link: "https://docs.google.com/document/d/1RNxLbpxCHq9bk4zD45yB-1kMCJ76Tvq6M6c-98_0k9k/edit", title: "The lethality of training on human feedback", preview: "Human operators are fallible, breakable, and manipulable.  Human raters make systematic errors - regular, compactly describable, predictable errors.  To faithfully learn....", date: "13th JAN 2024" },
+
         { link: "https://docs.google.com/document/d/1zjBKDtviu-NuGaZz2md8KvPRVicNLX5P6_fkmI3FVK4/edit?tab=t.0", title: "A Flawed Primer of my Experience in AI Alignment so far", preview: "My views on artificial intelligence developed early on in school, unintentionally. I had begun learning computer science, programming in C++ and instantly dismissed the idea of an AI overlord, that WE had designed, turning on us and conquering the world.....", date: "24th JUL 2024" },
-        { link: "https://www.lesswrong.com/posts/WNTGe87fHwDZMLqzW/the-disastrously-confident-and-inaccurate-ai", title: "The Disastrously Confident And Inaccurate AI", preview: "Pseudo-aligned mesa-optimizers may be a super difficult problem to address or it may be a kinda easy problem to address. But the truth of the matter is that the facts we have pertaining to them are few in number, in fact, we know very little about them truly....", date: "1st NOV 2022" }
+
+        { link: "https://www.lesswrong.com/posts/WNTGe87fHwDZMLqzW/the-disastrously-confident-and-inaccurate-ai", title: "The Disastrously Confident And Inaccurate AI", preview: "Pseudo-aligned mesa-optimizers may be a super difficult problem to address or it may be a kinda easy problem to address. But the truth of the matter is that the facts we have pertaining to them are few in number, in fact, we know very little about them truly....", date: "1st NOV 2022" },
+
+        { link: "https://medium.com/paperkin/should-art-pay-the-price-for-its-artists-b1f24d88103b", title: "Should Art Pay the Price for its Artists?", preview: "Van Gogh was depressed and alone.", date: "2nd AUG 2022" }
+
     ]
 
     const title = { front: BlogF, back: BlogB, centre: BlogCentre, color: '#C5D86D', top: -11.719, width: 46.875, height: 46.875, right: 27.344 };

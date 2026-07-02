044fda3104613cdf6eca02450245633482f46e81
Sharat Jacob Jacob <sharatjacob2@gmail.com>
2026-07-02 16:01:40 +0530

    revamped resume page a bit

--- src/resumePage/Resume.js
+++ src/resumePage/Resume.js
@@ -1,46 +1,79 @@
 import TitleEffect from "../TitleEffect";
-import { ReactComponent as ResumeF } from './RESUME f.svg';
-import { ReactComponent as ResumeB } from './RESUME b.svg';
-import { ReactComponent as ResumeCentre } from './Resume Centre Test.svg';
-import { ReactComponent as DownloadIcon } from './Download Icon.svg';
-import { ReactComponent as PageOne} from './PageOne.svg';
-import { ReactComponent as PageTwo} from './PageTwo.svg';
-import { ReactComponent as PageThree} from './PageThree.svg';
+import { ReactComponent as ResumeF } from "./RESUME f.svg";
+import { ReactComponent as ResumeB } from "./RESUME b.svg";
+import { ReactComponent as ResumeCentre } from "./Resume Centre Test.svg";
+
+import DownloadIcon from "./Download Icon.svg";
+
+import PageOne from "./PageOne.svg";
+import PageTwo from "./PageTwo.svg";
+import PageThree from "./PageThree.svg";
+
 import PageHeader from "../utils/PageHeader";
-
-
-
-import './Resume.css';
 import Footer from "../footer/Footer";
 
+import "./Resume.css";
 
 const Resume = () => {
-    document.body.classList.remove('hide-scrollbars');
+  document.body.classList.remove("hide-scrollbars");
 
-    const title = { front: ResumeF, back: ResumeB, centre: ResumeCentre, top: -21.094, left: -11.875, width: 55.313, height: 55.313 };
+  const title = {
+    front: ResumeF,
+    back: ResumeB,
+    centre: ResumeCentre,
+    top: -21.094,
+    left: -11.875,
+    width: 55.313,
+    height: 55.313,
+  };
 
-    return (
-        <div className="Resume-Page" style={{ backgroundColor: '#c44e2d' }}>
-            <TitleEffect title={title} />
-                  <PageHeader
-        title="A PROFESSIONAL HISTORY"
-        subtitle="Last revised · July 2026"
-        accentColor="#7F74C6"
+  return (
+    <div className="Resume-Page" style={{ backgroundColor: "#c44e2d" }}>
+      <TitleEffect title={title} />
+
+      <PageHeader
+        title="CURRENTLY STUDYING"
+        subtitle="BUT HERE'S A PROFESSIONAL HISTORY ANYWAY."
+        accentColor="#e08257"
       />
-            <a href="/Sharat Resume.pdf" download="Sharat Resume.pdf" className="download">
-                <button className="download-button">
-                    <DownloadIcon className="download-icon" />
-                    <p className="downloadtext">Download CV</p>
-                </button>
-            </a>
-            
-            <PageOne className="pageone"/>
-            <PageTwo className="pageone"/>
-            <PageThree className="pageone"/>
-            <Footer color = {'#e08257'}/>
-        </div>
 
-    );
-}
+      <a
+        href="/Sharat Resume.pdf"
+        download="Sharat Resume.pdf"
+        className="resume-download"
+      >
+        <img src={DownloadIcon} alt="" className="download-icon" />
+
+        <span>Download PDF ↗</span>
+      </a>
+      <div className="resume-meta">
+        Viewing 3 Pages • PDF • Updated July 2026
+      </div>
+      <div className="resume-stack">
+
+        <img
+          src={PageOne}
+          alt="Resume page 1"
+          className="resume-page"
+        />
+
+        <img
+          src={PageTwo}
+          alt="Resume page 2"
+          className="resume-page"
+        />
+
+        <img
+          src={PageThree}
+          alt="Resume page 3"
+          className="resume-page"
+        />
+
+      </div>
+
+      <Footer color="#e08257" />
+    </div>
+  );
+};
 
 export default Resume;
